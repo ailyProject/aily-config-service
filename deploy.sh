ff7ce434f8e289ff179434198d286b0fd700db85
stao <werewolf_st@hotmail.com>
2024-04-15 17:44:00 +0800

    调整

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,18 @@ sudo apt-get update && sudo apt-get install supervisor
 
 # 配置 supervisor
 SUPERVISOR_CONF_DIR="/etc/supervisor/conf.d"
-SUPERVISOR_CONF_FILE="ailyconf.conf"
+SUPERVISOR_CONF_FILE="supervisor/ailyconf.conf"
+
+echo "[program: ailyconf]" > $SUPERVISOR_CONF_FILE
+echo "command = $HOME/aily-config-service/venv/bin/python main.py" >> $SUPERVISOR_CONF_FILE
+echo "directory = $HOME/aily-config-service" >> $SUPERVISOR_CONF_FILE
+echo "user = root" >> $SUPERVISOR_CONF_FILE
+echo "autostart = true" >> $SUPERVISOR_CONF_FILE
+echo "autorestart = true" >> $SUPERVISOR_CONF_FILE
+echo "stdout_logfile = /tmp/ailyconf.log" >> $SUPERVISOR_CONF_FILE
+echo "stdout_logfile_maxbytes = 1MB" >> $SUPERVISOR_CONF_FILE
+echo "stderr_logfile = /tmp/ailyconf-error.log" >> $SUPERVISOR_CONF_FILE
+echo "stderr_logfile_maxbytes = 1MB" >> $SUPERVISOR_CONF_FILE
 
 sudo cp supervisor/$SUPERVISOR_CONF_FILE $SUPERVISOR_CONF_DIR
 sudo supervisorctl reload
