80b3ba9bc2da5c8438c21ab15e33aa09fc603ece
stao <werewolf_st@hotmail.com>
2024-06-25 14:45:18 +0800

    update

--- http_main.py
+++ http_main.py
@@ -4,12 +4,16 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Any
 
+from loguru import logger
+
 from utils.aily_ctl import AilyCtl
 
 
+aily_ctl = AilyCtl()
+
 class ResponseModel(BaseModel):
-    code: int
-    message: str
+    code: int = 200
+    message: str = "success"
     data: Any = None
 
 
@@ -21,7 +25,7 @@ async def get_logs(
     page: int = 1,
     perPage: int = 10,
 ):
-    aily_ctl = AilyCtl()
+    logger.debug(f"get_logs: page={page}, perPage={perPage}")
     aily_ctl.log_cur_page = page
     res = aily_ctl.get_logs(perPage)
 
