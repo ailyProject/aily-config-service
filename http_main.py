b0b4d6b28c8070079e3dbc7e68f03588a75d6104
stao <werewolf_st@hotmail.com>
2024-06-25 14:32:44 +0800

    update

--- http_main.py
+++ http_main.py
@@ -2,17 +2,19 @@ import uvicorn
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Any
 
 from utils.aily_ctl import AilyCtl
 
 
 class ResponseModel(BaseModel):
-    status: int = 200
-    messages: str = "Success"
-    data: any
+    code: int
+    message: str
+    data: Any = None
 
 
 app = FastAPI()
+
 
 @app.get("/api/v1/logs")
 async def get_logs(
@@ -22,8 +24,8 @@ async def get_logs(
     aily_ctl = AilyCtl()
     aily_ctl.log_cur_page = page
     res = aily_ctl.get_logs(perPage)
-    
-    return ResponseModel(data=res)
-    
+
+    return ResponseModel(data={"page": page, "perPage": perPage, "list": res})
+
 
 uvicorn.run(app, host="0.0.0.0", port=8888)
